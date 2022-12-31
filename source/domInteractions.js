ab8a80f9976bee031f7fb8e90f65cebd608c2c41
Marcin_Chudzik <zerone.platform@gmail.com>
2022-12-31 04:17:03 +0100

    Project started
    Mobile 1st:
    - Navigation is finished.
    - Section "Home" and "About" are finished.

--- source/domInteractions.js
+++ source/domInteractions.js
@@ -17,3 +17,34 @@ export const setFocusAndTitle = () => {
   heading.focus();
   document.title = heading.innerText;
 };
+
+// EVENT FUNCTIONS
+export const menuCross = event => {
+  const listOfBars = Array.from(document.querySelectorAll('[data-menu-bar]'));
+  const bar1 = listOfBars[0];
+  const bar2 = listOfBars[1];
+  const bar3 = listOfBars[2];
+
+  const crossIn = () => {
+    bar1.style.transform = 'skewY(45deg)';
+    bar1.style.top = '45%';
+    bar3.style.transform = 'skewY(-45deg)';
+    bar3.style.bottom = '45%';
+  };
+
+  const crossOut = () => {
+    bar1.style.transform = 'unset';
+    bar1.style.top = '0';
+    bar3.style.transform = 'unset';
+    bar3.style.bottom = '0';
+  };
+  
+
+  if (bar2.style.left === '100px') {
+    crossOut();
+    setTimeout(bar2.style.left = '0', 400);
+  } else {
+    bar2.style.left = '100px';
+    crossIn();
+  };
+};
