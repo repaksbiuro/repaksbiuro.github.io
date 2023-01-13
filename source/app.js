6de24c4f6006d78ea688ffd4b8bdd82dca5e30a7
Marcin_Chudzik <marcinchudzik01@gmail.com>
2023-01-13 22:16:29 +0100

    Contact form works.

--- source/app.js
+++ source/app.js
@@ -1,4 +1,5 @@
 import { mapListToDOMElements, setFocusAndTitle, displayMenu, displayScrollBtn, displayOfferDetails } from "./domInteractions.js";
+import { sendFormToAPI } from "./requests.js";
 
 class Repaks {
     constructor() {
@@ -29,7 +30,26 @@ class Repaks {
         );
 
         this.viewElements.menuBtn.addEventListener('click', displayMenu);
+        this.viewElements.contactForm.addEventListener('submit', this.sendEmail);
         window.addEventListener('scroll', displayScrollBtn);
+    };
+
+    sendEmail = event => {
+        event.preventDefault();
+        let name = event.target.fname.value;
+
+        if (name.length <= 2) {
+            window.alert('Pole "Imię" musi posiadać conajmniej 3 znaki.')
+            return false
+        };
+
+        let templateParams = {
+            fname: name,
+            contact_email: event.target.contact_email.value,
+            message: event.target.message.value,
+        };
+
+        sendFormToAPI(templateParams);
     };
 };
 
---
+++ source/requests.js
@@ -0,0 +1,9 @@
+export const sendFormToAPI = (templateParams) => {
+    emailjs.send("contact_service", "contactForm", templateParams, "LPKOlqGroizWveNXp")
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 300) {
+          document.getElementById("formSendBtn").innerText = "Wysłano";
+        };
+        window.alert('Twój formularz został wysłany poprawnie. Sprawdź skrzynkę e-mail.');
+    });
+};
